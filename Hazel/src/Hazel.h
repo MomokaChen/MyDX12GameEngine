bfec65fe90fee5b9a711d636a41656562dc162cc
moka <3129201524@qq.com>
2026-03-26 06:37:44 +0800

    Log

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,7 +3,7 @@
 // for use by hazel applications
 
 #include "Hazel/Application.h"
-
+#include "Hazel/Log.h"
 // ---Entry point-------------
 #include "Hazel/EntryPoint.h"
 // ---------------------------
